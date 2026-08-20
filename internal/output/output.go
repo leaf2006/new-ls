5c5fdec8fca7fcbbd4661314772eaa916e5e219d
leaf2006 <yeshuo20061031@outlook.com>
2026-08-20 13:49:23 +0800

    更新：如果文件夹为空，nls将不输出任何东西

--- internal/output/output.go
+++ internal/output/output.go
@@ -15,30 +15,32 @@ var TitleColor = color.New(color.FgHiGreen, color.Bold)
 
 func NormalOutput() {
 	rows := core.Rows
-	isSimpleColor := false
+	if len(rows) > 0 {
 
-	TitleColor.Printf("%-10s    %-16s    %-*s    %s\n", "Mode", "LastWriteTime", core.MaxSizeLen, "Size", "Name")
-	dashSize := strings.Repeat("-", core.MaxSizeLen)
-	TitleColor.Printf("%-10s    %-16s    %-*s    %s\n", "----", "-------------", core.MaxSizeLen, dashSize, "----")
+		isSimpleColor := false // 控制是否为简单输出，当前为正常输出，所以是false
+		TitleColor.Printf("%-10s    %-16s    %-*s    %s\n", "Mode", "LastWriteTime", core.MaxSizeLen, "Size", "Name")
+		dashSize := strings.Repeat("-", core.MaxSizeLen)
+		TitleColor.Printf("%-10s    %-16s    %-*s    %s\n", "----", "-------------", core.MaxSizeLen, dashSize, "----")
 
-	for _, row := range rows {
-		var namePrinter *color.Color
-		fmt.Printf("%-10s    ", row.Mode)
-		fmt.Printf("%-16s    ", row.Time)
-		fmt.Printf("%*s    ", core.MaxSizeLen, row.Size)
+		for _, row := range rows {
+			var namePrinter *color.Color
+			fmt.Printf("%-10s    ", row.Mode)
+			fmt.Printf("%-16s    ", row.Time)
+			fmt.Printf("%*s    ", core.MaxSizeLen, row.Size)
 
-		namePrinter = render.ColorFormatter(row.RawFile, row.IsDir, isSimpleColor)
-		var EnableIcon string
-		if config.Global.Icon == true {
-			EnableIcon = row.Icon + " "
-		} else {
-			EnableIcon = ""
-		}
-		if namePrinter != nil {
-			namePrinter.Printf("%s%s", EnableIcon, row.Name)
-			fmt.Printf("\n") // 消除蓝色底色色块溢出的问题
-		} else {
-			fmt.Printf("%s%s\n", EnableIcon, row.Name)
+			namePrinter = render.ColorFormatter(row.RawFile, row.IsDir, isSimpleColor)
+			var EnableIcon string
+			if config.Global.Icon == true {
+				EnableIcon = row.Icon + " "
+			} else {
+				EnableIcon = ""
+			}
+			if namePrinter != nil {
+				namePrinter.Printf("%s%s", EnableIcon, row.Name)
+				fmt.Printf("\n") // 消除蓝色底色色块溢出的问题
+			} else {
+				fmt.Printf("%s%s\n", EnableIcon, row.Name)
+			}
 		}
 	}
 }
